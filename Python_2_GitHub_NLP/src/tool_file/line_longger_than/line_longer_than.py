03978516e95fcb6640e6213e4001fe11b71bdcfc
unknown <lijianbo@zhongbiao.mobi>
2016-05-09 10:44:13 +0800

    111

---
+++ Python_2_GitHub_NLP/src/tool_file/line_longger_than/line_longer_than.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+'''
+Created on 2016年5月9日
+@author: 李健博
+程序作用：
+输出一行字数大于等于7的行
+'''
+from __future__ import division
+from __future__ import unicode_literals
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')  # @UndefinedVariable
+
+fileDir = "newFile.txt"
+
+lineLength = 7
+outFileName = "bigThan"+str(lineLength)+".txt"
+
+lis_write = []
+with open(fileDir, 'rb') as somefile:
+    for line in somefile:
+        line = line.decode('utf-8')
+        line = line.strip()  # 去除换行符号
+        if len (line) >= lineLength:
+            lis_write.append(line)
+
+with open(outFileName,'wb') as somefile:
+    for line in lis_write: # 不会自动换行
+        line+="\r\n"
+        somefile.write(line)
+
+print "down"
+
+        
+        
+
+
+         
+
+        
---
+++ Python_2_GitHub_NLP/src/tool_file/minus/minus.py
@@ -0,0 +1,51 @@
+# coding=utf-8
+'''
+Created on 2016年5月9日
+@author: 李健博
+程序作用：
+从bigFile中删除smallFile中出现的行
+'''
+from __future__ import division
+from __future__ import unicode_literals
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')  # @UndefinedVariable
+
+
+bigFile="new_dict_no_duplicate.txt"
+smallFile="longword.txt"
+newFile="newFile.txt"
+
+lis_write=[]
+with open(bigFile,'rb') as somefile:
+    for line in somefile:
+        line = line.decode('utf-8')
+        line= line.strip() # 去除换行符号
+        lis_write.append(line)
+        
+smallSet=set()
+with open(smallFile,'rb') as somefile:
+    for line in somefile:
+        line = line.decode('utf-8')
+        line= line.strip() # 去除换行符号
+        smallSet.add(line)
+
+with open(newFile,'wb') as somefile:
+    for line in lis_write: # 不会自动换行
+        if line not in smallSet:
+            line+="\r\n"
+            somefile.write(line)
+print "down"
+        
+
+
+         
+
+        
+        
+        
+
+
+         
+
+        
