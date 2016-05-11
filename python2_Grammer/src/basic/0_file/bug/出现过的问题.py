f3df2ce457650932b163d2e3d83ef180f58acffa
unknown <lijianbo@zhongbiao.mobi>
2016-05-11 10:39:22 +0800

    11

--- python2_Grammer/src/basic/0_file/bug/出现过的问题.py
+++ python2_Grammer/src/basic/0_file/bug/出现过的问题.py
@@ -2,15 +2,18 @@
 '''
 本地硬盘和u盘的读取速度可能相差50倍
 '''
+
 '''
-字符串开头没有加r
+1:字符串开头没有加r
 如：
 f1=open(r'a.txt' % (2),'r',encoding= 'utf-8')
 
 在Python的string前面加上‘r’
 是为了告诉编译器这个string是个raw string,不要转意backslash '\
 '''
+
 '''
+2:文件编码不对
 文件一定要使用utf-8 无bom
 不然读进来的文件第一行会有错误
 python自动写入的文件是utf-8 无bom的
